eb515c988724a46192b17e29eb071142ebd35ed2
Rhama Arya Wibawa <a.rhama@jmtech.co.jp>
2020-07-22 13:46:38 +0900

    - Minimum settings for empty Android project. Unrunnable but should be no errors.

---
+++ app/src/main/java/com/arya199/n3drill/N3DrillApplication.kt
@@ -0,0 +1,12 @@
+package com.arya199.n3drill
+
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+open class N3DrillApplication: DaggerApplication() {
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        TODO("Not yet implemented")
+    }
+
+}
