03b2a4372eec92132a2c94b0e0b75ab9d2203e89
Rhama Arya Wibawa <a.rhama@jmtech.co.jp>
2020-07-22 14:09:12 +0900

    - Minimum Dagger component to pass the project build.

--- app/src/main/java/com/arya199/n3drill/N3DrillApplication.kt
+++ app/src/main/java/com/arya199/n3drill/N3DrillApplication.kt
@@ -1,12 +1,12 @@
 package com.arya199.n3drill
 
+import com.arya199.n3drill.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 open class N3DrillApplication: DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        TODO("Not yet implemented")
+        return DaggerApplicationComponent.factory().create(applicationContext)
     }
-
 }
---
+++ app/src/main/java/com/arya199/n3drill/di/ApplicationComponent.kt
@@ -0,0 +1,18 @@
+package com.arya199.n3drill.di
+
+import android.content.Context
+import com.arya199.n3drill.N3DrillApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AndroidInjectionModule::class])
+interface ApplicationComponent: AndroidInjector<N3DrillApplication> {
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance applicationContext: Context): ApplicationComponent
+    }
+}
