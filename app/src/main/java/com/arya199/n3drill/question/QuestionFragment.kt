299758a3be667662d6489064aef18bba0e4c0a86
Rhama Arya Wibawa <rhama.arya@gmail.com>
2020-09-27 12:06:35 +0900

    - Minimum working example for item click on an list item.

--- app/src/main/java/com/arya199/n3drill/question/QuestionFragment.kt
+++ app/src/main/java/com/arya199/n3drill/question/QuestionFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.arya199.n3drill.data.Question
@@ -50,6 +51,11 @@ class QuestionFragment: DaggerFragment() {
         if (viewModel != null) {
             listAdapter = QuestionListAdapter(viewModel)
             viewDataBinding.questionList.adapter = listAdapter
+            listAdapter.setOnItemClickListener(object: QuestionListAdapter.OnItemClickListener {
+                override fun onClick(data: Question) {
+                    Toast.makeText(context, data.Bogus_1, Toast.LENGTH_LONG).show()
+                }
+            })
         }
     }
 }
--- app/src/main/java/com/arya199/n3drill/question/QuestionListAdapter.kt
+++ app/src/main/java/com/arya199/n3drill/question/QuestionListAdapter.kt
@@ -6,21 +6,29 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.arya199.n3drill.data.Question
-import com.arya199.n3drill.databinding.QuestionItemBinding
+import com.arya199.n3drill.databinding.QuestionListItemBinding
 
 class QuestionListAdapter(private val questionViewModel: QuestionViewModel): ListAdapter<Question,
         QuestionListAdapter.ViewHolder>(QuestionDiffCallback()) {
 
+    lateinit var listener: OnItemClickListener
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        setOnItemClickListener(listener)
         return ViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
         holder.bind(item)
+        holder.itemView.setOnClickListener { listener.onClick(item) }
     }
 
-    class ViewHolder private constructor(val binding: QuestionItemBinding):
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
+    class ViewHolder private constructor(private val binding: QuestionListItemBinding):
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Question) {
@@ -31,7 +39,7 @@ class QuestionListAdapter(private val questionViewModel: QuestionViewModel): Lis
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = QuestionItemBinding.inflate(layoutInflater, parent, false)
+                val binding = QuestionListItemBinding.inflate(layoutInflater, parent, false)
                 return ViewHolder(binding)
             }
         }
@@ -45,6 +53,9 @@ class QuestionListAdapter(private val questionViewModel: QuestionViewModel): Lis
         override fun areContentsTheSame(oldItem: Question, newItem: Question): Boolean {
             return oldItem.Question == newItem.Question
         }
+    }
 
+    interface OnItemClickListener {
+        fun onClick(data: Question)
     }
 }
